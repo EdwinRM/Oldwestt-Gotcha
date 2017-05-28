f1679309df067a3ef55e3ea3ced51534f8e0aea3
EdwinRM <edwinrmondragon@gmail.com>
2017-05-28 18:47:07 -0500

    Update Enviar.php

--- paginas/Enviar.php
+++ paginas/Enviar.php
@@ -5,14 +5,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+<?php
+$mail = $_POST["mensaje"];
 
-$destino ="edwinrmondragon@gmail.com";
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$mensaje = $_POST["mensaje"];
-
-$contenido="Nombre: ".$nombre. "\nCorreo: ".$correo."\nMensaje: ".$mensaje;
-mail($destino, "Contacto", $contenido);
-echo '<script> alert("Correo enviado; Este atento a su correo le responderenmos lo mas pronto posible gracias.");
+//Titulo
+$titulo = $_POST["nombre"];
+//cabecera
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+//dirección del remitente 
+$headers .= "From: Geeky Theory < edwinrmondragon@gmail.com >\r\n";
+//Enviamos el mensaje a tu_dirección_email 
+$bool = mail("edwinrmondragon@gmail.com",$titulo,$mail,$headers);
+if($bool){
+    echo "Mensaje enviado";
+}else{
+    echo "Mensaje no enviado";
+}
+?>
 
 
