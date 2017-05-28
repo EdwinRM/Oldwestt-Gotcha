85f5d5941a69719ae93aac865d2188895b44c227
EdwinRM <edwinrmondragon@gmail.com>
2017-05-28 18:07:55 -0500

    Update Enviar.php

--- paginas/Enviar.php
+++ paginas/Enviar.php
@@ -14,3 +14,4 @@ $mensaje = $_POST["mensaje"];
 $contenido="Nombre: ".$nombre. "\nCorreo: ".$correo."\nMensaje: ".$mensaje;
 mail($destino, "Contacto", $contenido);
 echo '<script> alert("Correo enviado; Este atento a su correo le responderenmos lo mas pronto posible gracias.");</script>';
+header('Location: Conocenos.php');
