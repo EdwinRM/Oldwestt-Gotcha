a548d714ad98a20bea09ac776dcf7bf3fc0b40e7
EdwinRM <edwinrmondragon@gmail.com>
2017-05-28 17:05:16 -0500

    Update home.php

--- home.php
+++ home.php
@@ -30,9 +30,9 @@
                 <label class="icon-menu"for="menu-bar"></label> 
                 <nav class="menu">
                     <a href="Index.php">Inicio</a>
-                    <a href="Paginas/Conocenos.php">Conocenos</a>
-                    <a href="Paginas/Servicios.php">Servicios</a>
-                    <a href="Paginas/Contacto.php">Contacto</a>                                              
+                    <a href="/Paginas/Conocenos.php">Conocenos</a>
+                    <a href="/Paginas/Servicios.php">Servicios</a>
+                    <a href="/Paginas/Contacto.php">Contacto</a>                                              
                 </nav>
 
             </div>                
