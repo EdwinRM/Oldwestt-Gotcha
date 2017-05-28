cf63662f939ccd0196418734d9bcdbb53970000b
EdwinRM <edwinrmondragon@gmail.com>
2017-05-28 16:47:41 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="with=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">     
+        <link rel="stylesheet" href="css/style.css" type="text/css">             
+        <link rel="stylesheet" href="flexslider.css" type="text/css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery.flexslider.js"></script>
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+        <title>Old West Gotcha Cadereyta</title>       
+    </head>
+    <script type="text/javascript" charset="utf-8">
+     $(window).load(function () {
+         $('.flexslider').flexslider({
+             touch: true,
+             pauseOnAction: false,
+             pauseOnHover: false,
+         });
+     });
+    </script>
+    <body>
+
+        <header>               
+            <div id="color">  </div> 
+            <div class="contenedor">                    
+                <a href="Index.php" id="logo"><img src="img/logochido.png" alt=""></a>
+                <h1 class="">Old West Gotcha Cadereyta</h1>
+                <input type="checkbox" id="menu-bar">
+                <label class="icon-menu"for="menu-bar"></label> 
+                <nav class="menu">
+                    <a href="Index.php">Inicio</a>
+                    <a href="Paginas/Conocenos.php">Conocenos</a>
+                    <a href="Paginas/Servicios.php">Servicios</a>
+                    <a href="Paginas/Contacto.php">Contacto</a>                                              
+                </nav>
+
+            </div>                
+        </header>     
+        <div id="color2">  </div> 
+        <div class="flexslider">
+            <ul class="slides">
+                <li>
+                    <img src="img/19.jpg" alt="">
+                    <section class="flex-caption">
+                        <p class="">Old West Gotcha Cadereyta!</p><br>                  
+                    </section>
+                </li>
+                <li>
+                    <img src="img/15.jpg" alt="">
+                    <section class="flex-caption">                    
+                         <p>Zona para Acampar!</p>                       
+                    </section>
+                </li>
+                <li>
+                    <img src="img/16.jpg" alt="">
+                    <section class="flex-caption">
+                        <p>Adrenalina al 100%!</p>
+                    </section>
+                </li>
+
+                <li>
+                    <img src="img/20.jpg" alt="">
+                    <section class="flex-caption">
+                        <p>Divierte con tus amigos!!</p>
+                    </section>
+                </li>
+            </ul>
+        </div>    
+
+        <section id="two">                         
+            <ul>
+                <div class="imagenes" id="imagen1">
+                    <div id="color1">
+                        <br><br>
+                        <a href="paginas/Conocenos.php"class="icon-users"for="menu-bar"></a>
+                    </div>   
+                    <li class="textfotos"><p>Mucha divercion y adrenalina  conocenos! </p></li>    
+                </div>  <br>           
+                <div class="imagenes" id="imagen2">
+                        <div id="color1">
+                            <br><br>
+                            <a href="paginas/Servicios.php"class="icon-basket"for="menu-bar"></a>
+                    </div> 
+                    
+                    <li class="textfotos"><p>Nuestros Paquetes y Servicios a tu alcanze! </p></li>
+                </div><br>   
+                <div class="imagenes" id="imagen3">
+                        <div id="color1">
+                            <br><br>
+                            <a href="paginas/Contacto.php"class="icon-location"for="menu-bar"></a>
+                    </div>
+                     
+                    <li class="textfotos"><p>Contactanos y organizamos tu propio evento!</p></li>
+                </div>                     
+            </ul>                                                  
+        </section>
+
+        <section id="tree">                   
+            <div id="contenedor-video-youtube">
+                <div id="video-youtube">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/FqD6fmI_jQ0?ecver=1" frameborder="0" allowfullscreen></iframe>                     
+                </div>                
+            </div>
+        </section> 
+        <br>
+       <footer>
+        <div id="contenedor-footer">  
+           <div id="color2"></div>       
+           <div class="social"><span>                   
+                    <ol>
+                        <li><a href="https://www.facebook.com/OldWestGotchaCadereyta/"  target= "_blank" class="icon-facebook"></a></li>
+                        <li><a href="https://www.instagram.com/oldwestgotchacadereyta/"   target= "_blank" class="icon-instagram"></a></li>
+                        <li><a href="https://www.youtube.com/channel/UCMycmCKRUHEnkJh9a39d4Zw"   target= "_blank" class="icon-youtube"></a></li>
+                    </ol>
+                </span>
+            </div>
+           
+                <p class="textofooter"> Mucha divercion y adrenalina te esperan en Old West Gotcha Cadereyta <br> 
+                    perfecto para pasarla con tus amigos o familiares, vivan una experiencia <br> a la viejo oeste.
+                    Sensacionales juegos de gotcha y un agradable ambiente<br> ¡Organiza tu evento con nosotros! <br><br>Horario: 12:00 - 19:00 fines de semana </p>  
+                
+                <p class="textodireccion"> Estamos ubicados en:<br>Pilancon, 76500 Cadereyta de Montes, Qro, México <br> ✆  442 361 7779</p>        
+         </div>
+           <div id="color3">
+            <p>Copyright © 2017 Old West Gotcha Cadereyta/ All rights reserved.</p>
+            <a href="Paginas/Logindeacceso.php">Adminstrador</a>
+           </div>
+      </footer>
+    </body>
+
+</html>
