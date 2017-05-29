42371dbbb68b8eca9c93e737cb4d563e9d35dc5d
EdwinRM <edwinrmondragon@gmail.com>
2017-05-28 23:09:12 -0500

    Update Enviar.php

--- paginas/Enviar.php
+++ paginas/Enviar.php
@@ -6,21 +6,17 @@
  * and open the template in the editor.
  */
 <?php
-$mail = $_POST["mensaje"];
-
-//Titulo
-$titulo = $_POST["nombre"];
-//cabecera
-$headers = "MIME-Version: 1.0\r\n"; 
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-//dirección del remitente 
-$headers .= "From: Geeky Theory < edwinrmondragon@gmail.com >\r\n";
-//Enviamos el mensaje a tu_dirección_email 
-$bool = mail("edwinrmondragon@gmail.com",$titulo,$mail,$headers);
-if($bool){
-    echo "Mensaje enviado";
-}else{
-    echo "Mensaje no enviado";
+$mensaje = $_POST["mensaje"];
+$correo = $_POST["correo"];
+// configuramos la cabecera que llevara el correo
+$asunto    = 'Prueba de SMTP local';
+$cabeceras = 'From: remitente@dominio.com' . "\r\n" .
+             'Reply-To: remitente@dominio.com' . "\r\n" .
+             'X-Mailer: PHP/' . phpversion();   
+if(mail($correo, $asunto, $mensaje, $cabeceras)) {
+    echo "Enviado";
+} else {
+    echo 'Error al enviar mensaje';
 }
 ?>
 
