2ab16df98197b53d2f61baea5be7b976925bfb02
EdwinRM <edwinrmondragon@gmail.com>
2017-05-28 23:10:40 -0500

    Update Enviar.php

--- paginas/Enviar.php
+++ paginas/Enviar.php
@@ -10,8 +10,8 @@ $mensaje = $_POST["mensaje"];
 $correo = $_POST["correo"];
 // configuramos la cabecera que llevara el correo
 $asunto    = 'Prueba de SMTP local';
-$cabeceras = 'From: remitente@dominio.com' . "\r\n" .
-             'Reply-To: remitente@dominio.com' . "\r\n" .
+$cabeceras = 'From: edwinrmondragon@gmail.com' . "\r\n" .
+             'Reply-To: edwinrmondragon@gmail.com' . "\r\n" .
              'X-Mailer: PHP/' . phpversion();   
 if(mail($correo, $asunto, $mensaje, $cabeceras)) {
     echo "Enviado";
