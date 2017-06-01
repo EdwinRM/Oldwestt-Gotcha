78ed490f17bded474d5b68b1711b23d54b7d0818
YugiKitsune <inu-chan_100@hotmail.com>
2017-05-31 20:50:02 -0500

    Add files via upload
    Se sube el archivo contacto

--- paginas/Contacto.php
+++ paginas/Contacto.php
@@ -1,5 +1,7 @@
 <?php
 include ('header.php');
+include ('funciones.php');
+$Funciones= new funciones();
 ?>
 <html lang="en">
     <head>
@@ -13,7 +15,8 @@ include ('header.php');
     <body>  
                 
                <section id="CUERPO">
-                   <p style="color: white;" class="texto-conacto">Estamos ubicados en: <br>Pilancon, 76500 Cadereyta de Montes, Qro, México <br>✆ 442 361 7779 </p>                               
+                   <?php $campo='telefono';  $dato=$Funciones->get_info_datos_empresa_by_campo($campo);?>
+                   <p style="color: white;" class="texto-conacto">Estamos ubicados en: <br>Pilancon, 76500 Cadereyta de Montes, Qro, México <br>✆ <?php echo $dato;?> </p>                 
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7465.258129442286!2d-99.80500332054042!3d20.68466479909863!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjDCsDQwJzU4LjAiTiA5OcKwNDcnNTguNyJX!5e0!3m2!1ses-419!2s!4v1494712493248" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>                     
             <form action="Enviar.php" method="post">          
                <p style="text-align: center; font-size:16px;">Mandanos un correo con los datos de tu evento</p>   
